20c7e2b5fe6c665f9bbb68ed843899f14331b1c0
suraaloqaily <aloqailysura@gmail.com>
2020-06-18 17:02:18 +0300

    You can now play X/O without any results

--- app.js
+++ app.js
@@ -1,15 +1,75 @@
-var sqr1 = document.getElementsById("sqr1")
-var sqr2 = document.getElementsById("sqr2")
-var sqr3 = document.getElementsById("sqr3")
-var sqr4 = document.getElementsById("sqr4")
-var sqr5 = document.getElementsById("sqr5")
-var sqr6 = document.getElementsById("sqr6")
-var sqr7 = document.getElementsById("sqr7")
-var sqr8 = document.getElementsById("sqr8")
-var sqr9 = document.getElementsById("sqr9")
-var sqrs=[sqr1,sqr2,sqr3,sqr4,sqr5,sqr6,sqr7,sqr8,sqr]
-// for (i = 0; i < cell.length; i++) {
-//     cell[i].addEventListener("click", function(){
-// console.log( "cell")
-// })
-// }
+var statues;
+
+var sqr1 = document.getElementById("sqr1").addEventListener("click", myFunction1);
+var sqr2 = document.getElementById("sqr2").addEventListener("click", myFunction2);
+var sqr3 = document.getElementById("sqr3").addEventListener("click", myFunction3);
+var sqr4 = document.getElementById("sqr4").addEventListener("click", myFunction4);
+var sqr5 = document.getElementById("sqr5").addEventListener("click", myFunction5);
+var sqr6 = document.getElementById("sqr6").addEventListener("click", myFunction6);
+var sqr7 = document.getElementById("sqr7").addEventListener("click", myFunction7);
+var sqr8 = document.getElementById("sqr8").addEventListener("click", myFunction8);
+var sqr9 = document.getElementById("sqr9").addEventListener("click", myFunction9);
+
+function Set (){
+	if(statues=== undefined){
+		// console.log(1)
+		statues = "X"
+	}
+	else if(statues=== "X"){
+		// console.log(2)
+		statues = "O"
+	}
+	else if(statues === "O"){
+		// console.log(3)
+		statues = "X"
+	}
+}
+function myFunction1() {
+	// console.log(statues)
+  Set()
+	// console.log(statues)
+
+ document.getElementById("sqr1").innerHTML = statues;
+}
+function myFunction2() {
+	// console.log(statues)
+
+  Set()
+	// console.log(statues)
+
+ document.getElementById("sqr2").innerHTML = statues;
+}
+function myFunction3() {
+  Set()
+ document.getElementById("sqr3").innerHTML = statues;
+}
+function myFunction4() {
+  Set()
+
+ document.getElementById("sqr4").innerHTML = statues;
+}
+function myFunction5() {
+  Set()
+
+ document.getElementById("sqr5").innerHTML = statues;
+}
+function myFunction6() {
+  Set()
+
+document.getElementById("sqr6").innerHTML = statues;
+}
+function myFunction7() {
+  Set()
+
+ document.getElementById("sqr7").innerHTML = statues;
+}
+function myFunction8() {
+  Set()
+
+ document.getElementById("sqr8").innerHTML = statues;
+}
+function myFunction9() {
+  Set()
+
+ document.getElementById("sqr9").innerHTML = statues;
+}
