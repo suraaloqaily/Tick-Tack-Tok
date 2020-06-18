6d0e015bcb89fdee87a2a4bec57c86c461b316cf
suraaloqaily <aloqailysura@gmail.com>
2020-06-18 16:28:27 +0300

    creat table and trying to make event listner

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-index.html
+var sqr1 = document.getElementsById("sqr1")
+var sqr2 = document.getElementsById("sqr2")
+var sqr3 = document.getElementsById("sqr3")
+var sqr4 = document.getElementsById("sqr4")
+var sqr5 = document.getElementsById("sqr5")
+var sqr6 = document.getElementsById("sqr6")
+var sqr7 = document.getElementsById("sqr7")
+var sqr8 = document.getElementsById("sqr8")
+var sqr9 = document.getElementsById("sqr9")
+var sqrs=[sqr1,sqr2,sqr3,sqr4,sqr5,sqr6,sqr7,sqr8,sqr]
+// for (i = 0; i < cell.length; i++) {
+//     cell[i].addEventListener("click", function(){
+// console.log( "cell")
+// })
+// }
